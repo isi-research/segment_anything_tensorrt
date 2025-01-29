c1028a63d9af27cd6013cb399375e6178c1c9dc1
Pourya Shirazian <Pourya.Shirazian@intusurg.com>
2025-01-28 16:28:25 -0800

    changes made to fix the tensorRT conversion

--- sam_trt_inference.py
+++ sam_trt_inference.py
@@ -25,24 +25,24 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("use tensorrt to inference segment anything model")
     parser.add_argument("--img_path", type=str, default="images/truck.jpg", help="you want segment image")
     parser.add_argument("--sam_engine_file", type=str, default="weights/sam_default_prompt_mask.engine")
-    parser.add_argument("--embedding_engine_file", type=str, default="embedding_onnx/sam_default_embedding.engine")
+    parser.add_argument("--embedding_engine_file", type=str, default="weights/sam_default_embedding.engine")
     parser.add_argument("--gpu_id", type=int, default=0, help="use which gpu to inference")
     parser.add_argument("--batch_size", type=int, default=1, help="use batch size img to inference")
     args = parser.parse_args()
-    
+
     image = cv2.imread(args.img_path)
     orig_im_size = image.shape[:2]
     img_inputs = pre_processing(image)
     print(f'img input: {img_inputs.shape}')
-    
+
     # embedding init
     embedding_inference = TrtModel(engine_path=args.embedding_engine_file, gpu_id=args.gpu_id, max_batch_size=args.batch_size)
     # sam init
     sam_inference = TrtModel(engine_path=args.sam_engine_file, gpu_id=args.gpu_id, max_batch_size=20)
-    
+
     image_embedding = embedding_inference([img_inputs])[0].reshape(1, 256, 64, 64)
     print(f"img embedding: {image_embedding.shape}")
-    
+
     # Point prompt
     input_point = np.array([[500, 375]])
     input_label = np.array([1])
@@ -52,11 +52,12 @@ if __name__ == "__main__":
     onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
     onnx_has_mask_input = np.zeros(1, dtype=np.float32)
 
-    # print(image_embedding.shape)
-    # print(onnx_coord.shape)
-    # print(onnx_label.shape)
-    # print(onnx_mask_input.shape)
-    # print(onnx_has_mask_input.shape)
+    print("<<<prompt mask tensors shapes>>>")
+    print("image_embeddings: {} dtype = {}".format(image_embedding.shape, image_embedding.dtype))
+    print("point_coords: {} dtype = {}".format(onnx_coord.shape, onnx_coord.dtype))
+    print("point_labels: {} dtype = {}".format(onnx_label.shape, onnx_label.dtype))
+    print("mask_input: {} dtype = {}".format(onnx_mask_input.shape, onnx_mask_input.dtype))
+    print("has_mask_input: {} dtype = {}".format(onnx_has_mask_input.shape, onnx_has_mask_input.dtype))
 
     input = [image_embedding, onnx_coord, onnx_label, onnx_mask_input, onnx_has_mask_input]
     shape_map = {'image_embeddings': image_embedding.shape,
@@ -64,15 +65,15 @@ if __name__ == "__main__":
                 'point_labels': onnx_label.shape,
                 'mask_input': onnx_mask_input.shape,
                 'has_mask_input': onnx_has_mask_input.shape}
-    
+
     output = sam_inference(input, binding_shape_map=shape_map)
-    
+
     # print(output[0].shape)
     # print(output[1].shape)
-    
+
     low_res_logits = output[0].reshape(args.batch_size, -1).reshape(4, 256, 256)
     scores =  output[1].reshape(args.batch_size, -1).squeeze(0)
-    
+
     masks = mask_postprocessing(low_res_logits, orig_im_size, img_inputs.shape[2])
     masks = masks.numpy().squeeze(0)
     os.makedirs("results", exist_ok=True)
@@ -80,7 +81,7 @@ if __name__ == "__main__":
     #     # mask_image = show_mask(masks[i]*255)
     #     cv2.imwrite(f"results/trt_mask{i}.png", masks[i]*255)
     #     print(f"Generate results/trt_mask{i}.png")
-    
+
     for i, (mask, score) in enumerate(zip(masks, scores)):
         mask = mask > 0.0
         plt.figure(figsize=(10,10))
@@ -91,5 +92,4 @@ if __name__ == "__main__":
         plt.axis('off')
         plt.savefig(f"results/trt_mask{i}.png", bbox_inches='tight', pad_inches=0)
         print(f"generate: results/trt_mask{i}.png")
-        # plt.show() 
-    
+        # plt.show()
--- utils/common.py
+++ utils/common.py
@@ -28,7 +28,7 @@ class HostDeviceMem(object):
 
     def __repr__(self):
         return self.__str__()
-    
+
 class TrtModel():
     def __init__(self, engine_path, gpu_id=0, max_batch_size=1):
         print("TensorRT version: %s" % trt.__version__)
@@ -44,25 +44,26 @@ class TrtModel():
 
     @staticmethod
     def load_engine(trt_runtime, engine_path):
-        trt.init_libnvinfer_plugins(None, "")             
+        trt.init_libnvinfer_plugins(None, "")
         with open(engine_path, 'rb') as f:
             engine_data = f.read()
         engine = trt_runtime.deserialize_cuda_engine(engine_data)
         return engine
-    
+
     def allocate_buffers(self):
         inputs = []
         outputs = []
         bindings = []
         stream = cuda.Stream()
-        
+
         for binding in self.engine:
             if binding in  ["point_coords", "point_labels"]:
                 size = abs(trt.volume(self.engine.get_binding_shape(binding))) * self.max_batch_size
             else:
-                size = abs(trt.volume(self.engine.get_binding_shape(binding))) 
+                size = abs(trt.volume(self.engine.get_binding_shape(binding)))
             # print(f"binding: {binding}, size: {size}")
-            dtype = trt.nptype(self.engine.get_binding_dtype(binding))
+            binding_dtype = self.engine.get_binding_dtype(binding)
+            dtype = trt.nptype(binding_dtype)
             host_mem = cuda.pagelocked_empty(size, dtype)
             device_mem = cuda.mem_alloc(host_mem.nbytes)
             bindings.append(int(device_mem))
@@ -71,30 +72,30 @@ class TrtModel():
             else:
                 outputs.append(HostDeviceMem(host_mem, device_mem))
         return inputs, outputs, bindings, stream
-    
+
     def __call__(self, inf_in_list, binding_shape_map=None):
         self.cfx.push()
-        
+
         if binding_shape_map:
             self.context.set_optimization_profile_async
             for binding_name, shape in binding_shape_map.items():
                 binding_idx = self.engine[binding_name]
                 # print(f"binding_name: {binding_name}, binding_idx: {binding_idx}, shape: {shape}")
                 self.context.set_binding_shape(binding_idx, shape)
-        
+
         for i in range(len(self.inputs)):
             self.inputs[i].host = inf_in_list[i]
             cuda.memcpy_htod_async(self.inputs[i].device, self.inputs[i].host, self.stream)
-        
+
         self.context.execute_async_v2(bindings=self.bindings, stream_handle=self.stream.handle)
-        
+
         for i in range(len(self.outputs)):
             cuda.memcpy_dtoh_async(self.outputs[i].host, self.outputs[i].device, self.stream)
-        
+
         self.stream.synchronize()
         self.cfx.pop()
         return [out.host.copy() for out in self.outputs]
-    
+
     def __del__(self):
         self.cfx.pop()
         del self.cfx
